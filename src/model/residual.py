0ac8164b48978ef2c67d99dca8e985e5a8a9cf54
Ki-Knight <41099448+Ki-Knight@users.noreply.github.com>
2018-08-27 21:11:16 +0800

    Add files via upload

---
+++ src/model/residual.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Mon Aug 27 10:19 2018
+
+Author          ：Shaoshu Yang
+Email           : 13558615057@163.com
+Last edit date  : Aug 27 10:21 2018
+
+Southe East University Automation College, 211189 Nanjing China
+'''
+
+import torch
+
+class Residual(torch.nn.Module):
+    def __init__(self, chan_in, chan_out):
+        super(Residual, self).__init__()
+
+    def forward(self, x):
+
+    def backward():
