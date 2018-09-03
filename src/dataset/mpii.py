57e56662f35b9f469ceb81270e7e5fc2359c442b
Mi-Dora <1239988498@qq.com>
2018-09-03 22:42:36 +0800

    ???

--- src/dataset/mpii.py
+++ src/dataset/mpii.py
@@ -1,1 +1,14 @@
+'''
+    Created on Mon Sep 03 22:14 2018
+
+    Author          ：Yu Du
+    Email           : 1239988498@qq.com
+    Last edit date  :
+
+South East University Automation College, 211189 Nanjing China
+'''
+
+
+import torch
+# from torchvision import
 
