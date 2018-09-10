d4eefe84576390821305343a2601066dd7f4cdbf
K Knight <13558615057@163.com>
2018-09-10 09:56:14 +0800

    Merge branch 'master' of https://github.com/SEUAutoCVSIG/HPE

--- src/dataset/coco.py
+++ src/dataset/coco.py
@@ -69,13 +69,10 @@ class COCO(Dataset):
                  Required label and data
         '''
         # Read image
-        img = cv2.imread(self.root + self.ann_list[idx]["name"])
+        img = cv2.imread(self.root + "train/" + self.ann_list[idx]["name"])
         img /= 255.0
-
-        # Transform BGR to RGB, HWC to CHW
-        img = img[:, :, ::-1].transpose((2, 0, 1))
-
         img_h, img_w = img.shape(0), img.shape(1)
+
         new_w = int(img_w*min(self.img_size/img_h, self.img_size/img_w))
         new_h = int(img_h*min(self.img_size/img_h, self.img_size/img_w))
 
@@ -87,7 +84,10 @@ class COCO(Dataset):
         canvas = np.full((self.img_size, self.img_size, 3), 128)
         canvas[(self.img_size - new_h) // 2:(self.img_size - new_h) // 2 + new_h,
         (self.img_size - new_w) // 2:(self.img_size - new_w) // 2 + new_w, :] = img
-        canvas = torch.tensor(canvas).float()
+
+        # Transform BGR to RGB, HWC to CHW
+        canvas = canvas[:, :, ::-1].transpose((2, 0, 1))
+        canvas = torch.from_numpy(canvas).float().unsequeeze(0)
 
         # Label pre-processing
         label = torch.tensor(self.ann_list[idx]["bbox"]).float()
@@ -100,6 +100,46 @@ class COCO(Dataset):
 
     def __len__(self):
         return len(self.ann_list)
+
+    def show_dataset(self):
+        for i, anno in enumerate(self.ann_list):
+            img = cv2.imread(self.root + "train/" + anno["name"])
+            img_h, img_w = img.shape(0), img.shape(1)
+
+            new_w = int(img_w * min(self.img_size / img_h, self.img_size / img_w))
+            new_h = int(img_h * min(self.img_size / img_h, self.img_size / img_w))
+
+            pad_h = (self.img_size - new_h) // 2
+            pad_w = (self.img_size - new_w) // 2
+
+            # Image pre-processing
+            img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_CUBIC)
+            canvas = np.full((self.img_size, self.img_size, 3), 128)
+            canvas[(self.img_size - new_h) // 2:(self.img_size - new_h) // 2 + new_h,
+            (self.img_size - new_w) // 2:(self.img_size - new_w) // 2 + new_w, :] = img
+
+            # Label pre-processing
+            label = np.array(anno["bbox"])
+            label[:, 0] *= new_w / img_w
+            label[:, 1] *= new_h / img_h
+            label[:, 2] *= new_w / img_w
+            label[:, 3] *= new_h / img_h
+
+            for bbox in anno["bbox"]:
+                draw_rect(bbox, img)
+
+            cv2.imshow(img)
+
+def draw_rect(bbox, img):
+    coord1 = bbox[:2]
+    coord2 = bbox[2:]
+    cv2.rectangle(img, coord1, coord2, (0, 255, 0), 2)
+    return img
+
+
+
+
+
 
 if __name__ == '__main__':
     coco = COCO("C:/PycharmProjects/HPE/", "data/coco_anno.txt")
