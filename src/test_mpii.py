a6ff72080a49e2922794f3290e715f87b61f222b
Mi-Dora <1239988498@qq.com>
2018-09-26 23:24:00 +0800

    -

--- src/test_mpii.py
+++ src/test_mpii.py
@@ -42,7 +42,7 @@ def eval_SH():
     cuda = torch.cuda.is_available()
     Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-    for i in range(10):
+    for i in range(1, 21):
         mAPs = np.zeros((16, 2))
         for data, target in data_loader:
             data = Variable(data.type(Tensor))
@@ -50,5 +50,8 @@ def eval_SH():
             output = model(data)
             op = np.array(get_points(output))
             tg = np.array(get_points(target))
-            mAPs += eval_pckh('mpii', tg, op, )
+            mAPs += eval_pckh('mpii', tg, op, 0.05*i)
         mAPs /= len(data_loader)
+        with open('SH_mAP.txt', 'a+') as fp:
+            fp.write('thresh:   %f  mAP     %f\n' %(0.05*i, mAPs))
+
